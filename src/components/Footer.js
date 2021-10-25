ca0a608d7db613aad62bb72dcd1aceb7ad60abee
Big B <8c57e820-f3a7-4561-a664-fed7d85bdedc@4wrd.cc>
2021-10-25 15:25:35 -0400

    Links show up in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -43,7 +43,7 @@ const genTagData = tagsFromDB => {
 
 const genColumnData = tags => {
     return tags.map(tag => {
-        const genLinkText = tgTxt => `Web3 ${capitalize(tgTxt)} jobs`;
+        const genLinkText = tgTxt => `Web3 ${tgTxt} jobs`;
         return { linkText: genLinkText(tag), path: tagURL(tag) }
     })
 }
@@ -179,46 +179,16 @@ export default function Footer() {
     const [tags, setTags] = useState([]);
     const [errorText, setError] = useState("");
 
-export default function Footer() {
-  return (
-    <footer className="bg-gray-800" aria-labelledby="footer-heading">
-      <h2 id="footer-heading" className="sr-only">
-        Footer
-      </h2>
-      <div className="border-t border-gray-700 pt-8 lg:flex lg:items-center lg:justify-between xl:mt-0">
-          <div>
-            <h3 className="text-sm font-semibold text-gray-400 tracking-wider uppercase">
-              Subscribe to our newsletter
-            </h3>
-            <p className="mt-2 text-base text-gray-300">
-              The latest news, articles, and resources, sent to your inbox weekly.
-            </p>
-          </div>
-          <form className="mt-4 sm:flex sm:max-w-md lg:mt-0">
-            <label htmlFor="email-address" className="sr-only">
-              Email address
-            </label>
-            <input
-              type="email"
-              name="email-address"
-              id="email-address"
-              autoComplete="email"
-              required
-              className="appearance-none min-w-0 w-full bg-white border border-transparent rounded-md py-2 px-4 text-base text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white focus:border-white focus:placeholder-gray-400 sm:max-w-xs"
-              placeholder="Enter your email"
-            />
-            <div className="mt-3 rounded-md sm:mt-0 sm:ml-3 sm:flex-shrink-0">
-              <button
-                type="submit"
-                className="w-full bg-indigo-500 border border-transparent rounded-md py-2 px-4 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-indigo-500"
-              >
-                Subscribe
-              </button>
-            </div>
-        </footer>
-    )
-}
+    useEffect(() => {
+        getTagsFromDB().catch(console.error);
+    }, []);
 
+    // function to pull the data from the DB
+    const getTagsFromDB = async () => {
+        let { data: tags, error } = await getAllTags();
+        if (error) setError(error);
+        else setTags(tags)
+    };
 
     return (
         <footer className="bg-gray-800" aria-labelledby="footer-heading">
