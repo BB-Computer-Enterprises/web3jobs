0279192cf7267d23f1db64bc14c0fd9ba0b85b03
B <uibmjh8e@anonaddy.me>
2021-10-25 22:49:39 -0400

    Updated post Job Form tailwind v1 (needs to be hooked) I noticed you have made changes to the same form. No biggie checkout mine let me know your thoughts.

--- src/components/pages/PostAJobPage.js
+++ src/components/pages/PostAJobPage.js
@@ -1,108 +1,140 @@
-import { useState, useEffect } from "react";
-import { Formik, Form, useField } from 'formik';
-import * as Yup from 'yup';
 
-const MyTextInput = ({ label, ...props }) => {
-    // useField() returns [formik.getFieldProps(), formik.getFieldMeta()]
-    // which we can spread on <input>. We can use field meta to show an error
-    // message if the field is invalid and it has been touched (i.e. visited)
-    const [field, meta] = useField(props);
-    return (
-        <>
-            <label htmlFor={props.id || props.name}>{label}</label>
-            <input className="text-input" {...field} {...props} />
-            {meta.touched && meta.error ? (
-                <div className="error">{meta.error}</div>
-            ) : null}
-        </>
-    );
-};
+import { ExclamationCircleIcon } from '@heroicons/react/solid'
 
 const PostAJobPage = () => {
 
-    const formikForm = () => {
-        return (
-            // TODO: add validation for application email
-            <Formik
-                initialValues={{
-                    contactEmail: '',
-                    companyName: '',
-                    jobTitle: '',
-                    description: '',
-                    applicationURL: '',
-                }}
-                validationSchema={Yup.object({
-                    contactEmail: Yup.string().email('Invalid email address').required('Required'),
-                    companyName: Yup.string().required('Company name Required'),
-                    jobTitle: Yup.string().required('Job title Required'),
-                    description: Yup.string().min(20, 'That\'s a pretty short description, dont you think 🤔?').required('Description Required'),
-                    applicationURL: Yup.string().required('Application URL Required'),
-                })}
-                onSubmit={(values, { setSubmitting }) => {
-                    setTimeout(() => {
-                        alert(JSON.stringify(values, null, 2));
-                        setSubmitting(false);
-                    }, 400);
-                }}
-            >
-                <Form>
-                    <MyTextInput
-                        label="Your Email"
-                        name="contactEmail"
-                        type="email"
-                    />
-
-                    <MyTextInput
-                        label="Company name"
-                        name="companyName"
-                        type="text"
-                    />
-
-                    <MyTextInput
-                        label="Job title"
-                        name="jobTitle"
-                        type="text"
-                    />
-
-                    <MyTextInput
-                        label="Job description"
-                        name="description"
-                        type="text"
-                    />
-
-                    <MyTextInput
-                        label="Apply URL (or email)"
-                        name="applicationURL"
-                        type="text"
-                    />
-
-                    <button type="submit">Post your job</button>
-                </Form>
-            </Formik>
-        );
-    }
 
     return (
-        <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div className={"w-screen fixed flex flex-col min-h-screen bg-gray-500"}>
-                <header
-                    className={
-                        "flex justify-between items-center px-4 h-16 bg-gray-900"
-                    }
-                >
-                    <span
-                        className={
-                            "text-2xl sm:text-4xl text-white border-b font-sans"
-                        }
-                    >
-                        Post A Job
-                    </span>
-                </header>
-                <div>
-                    {formikForm()}
+        <>
+            <div className="hidden sm:block" aria-hidden="true">
+                <div className="py-5">
+                    <div className="border-t border-gray-200" />
                 </div>
             </div>
-        </div>
+
+            <div className="mt-10 sm:mt-0">
+                <div className="md:grid md:grid-cols-3 md:gap-6">
+                    <div className="md:col-span-1">
+                        <div className="px-4 sm:px-0">
+                            <h3 className="text-lg font-medium leading-6 text-gray-900">Post Job</h3>
+                            <p className="mt-1 text-sm text-gray-600">Your only a few steps away from posting a job listing</p>
+                        </div>
+                    </div>
+                    <div className="mt-5 md:mt-0 md:col-span-2">
+                        <form action="#" method="POST">
+                            <div className="shadow overflow-hidden sm:rounded-md">
+                                <div className="px-4 py-5 bg-white sm:p-6">
+                                    <div className="grid grid-cols-6 gap-6">
+
+                                        <div className="col-span-6 sm:col-span-4">
+                                            <label htmlFor="email-address" className="block text-sm font-medium text-gray-700">
+                                                Email Address
+                                            </label>
+                                            <input
+                                                type="text"
+                                                name="email-address"
+                                                id="email-address"
+                                                autoComplete="email"
+                                                className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                                                placeholder="you@example.com"
+                                                aria-invalid="false"
+
+                                            />
+
+                                        </div>
+
+
+
+                                        <div className="col-span-6 sm:col-span-3">
+                                            <label htmlFor="first-name" className="block text-sm font-medium text-gray-700">
+                                                Company Name
+                                            </label>
+                                            <input
+                                                type="text"
+                                                name="first-name"
+                                                id="first-name"
+                                                autoComplete="given-name"
+                                                className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                                            />
+
+
+
+                                        </div>
+
+                                        <div className="col-span-6 sm:col-span-3">
+                                            <label htmlFor="last-name" className="block text-sm font-medium text-gray-700">
+                                                Job Title
+                                            </label>
+                                            <input
+                                                type="text"
+                                                name="last-name"
+                                                id="last-name"
+                                                autoComplete="family-name"
+                                                className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                                            />
+
+
+
+
+                                        </div>
+
+
+
+                                        <div className="col-span-6 sm:col-span-3">
+                                            <label htmlFor="about" className="block text-sm font-medium text-gray-700">
+                                                Job Description
+                                            </label>
+                                            <div className="mt-1">
+                                                <textarea
+                                                    id="about"
+                                                    name="about"
+                                                    rows={3}
+                                                    className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"
+                                                    placeholder="description"
+                                                    defaultValue={''}
+                                                />
+                                            </div>
+                                            <p className="mt-2 text-sm text-gray-500">
+                                                Brief description for your profile. URLs are hyperlinked.
+                                            </p>
+                                        </div>
+
+
+
+                                        <div className="col-span-6 sm:col-span-6 lg:col-span-2">
+                                            <label htmlFor="username" className="block text-sm font-medium text-gray-700">
+                                                Apply You URL
+                                            </label>
+                                            <div className="mt-1 flex rounded-md shadow-sm">
+                                                <span className="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 sm:text-sm">
+                                                    web3jobs.com/
+                                                </span>
+                                                <input
+                                                    type="text"
+                                                    name="username"
+                                                    id="username"
+                                                    autoComplete="username"
+                                                    className="flex-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full min-w-0 rounded-none rounded-r-md sm:text-sm border-gray-300"
+                                                />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                                    <button
+                                        type="submit"
+                                        className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                    >
+                                        Save
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div >
+            </div >
+
+        </>
     );
 };
 
