67866ca0a2090c895d73cba60b561e23d517b405
B <uibmjh8e@anonaddy.me>
2021-10-26 22:00:57 -0400

    Updated All Jobs UI - Still Needs Work

--- src/components/JobsList.js
+++ src/components/JobsList.js
@@ -1,6 +1,7 @@
 
 import { Link } from "react-router-dom";
 import { capitalize, makeFriendlyUrl } from "@util/sanitize";
+import { CalendarIcon, ChevronRightIcon } from '@heroicons/react/solid'
 import {
     COMPANY_NAME,
     JOBS_URL,
@@ -19,26 +20,62 @@ const generateLinkURL = job => {
 
 const JobsList = ({ jobs, isLoading, tag }) => {
     return (
-        <div>
-            <div
-                className={`p-2 flex-grow grid gap-2 ${jobs.length ? "auto-rows-min" : ""
-                    } grid-cols-1 h-2/3 first:mt-8`}
-            >
+        <div className="bg-white shadow overflow-hidden sm:rounded-md">
+            <ul role="list" className="divide-y divide-gray-200">
+
                 {jobs.length ? (
                     jobs.map((job) => (
-                        <div className="bg-white px-4 py-5 border-b border-gray-200 sm:px-6" key={job[JOB_ID]} >
-                            <Link to={{ pathname: generateLinkURL(job), state: { job } }}><h1 className="text-lg leading-6 font-medium text-gray-900">Job Title: {job[JOB_TITLE]}</h1></Link>
-                            <p className="mt-1 text-sm text-gray-500">Created: {new Date(job[JOB_DATE_POSTED]).toDateString()}</p>
-                            <p className="mt-1 text-sm text-gray-500">Description: {job[JOB_DESCRIPTION]}</p>
-                        </div>
+                        <li key={job[JOB_ID]}>
+                            <Link to={{ pathname: generateLinkURL(job), state: { job } }}><a className="block hover:bg-gray-50">
+                                <div className="px-4 py-4 flex items-center sm:px-6">
+                                    <div className="min-w-0 flex-1 sm:flex sm:items-center sm:justify-between">
+                                        <div className="truncate">
+                                            <div className="flex text-sm"></div>
+
+                                            <Link to={{ pathname: generateLinkURL(job), state: { job } }}><h1 className="font-medium text-indigo-600 truncate">Job Title: {job[JOB_TITLE]}</h1></Link>
+
+                                            <p className="ml-1 flex-shrink-0 font-normal text-gray-500">Created: {new Date(job[JOB_DATE_POSTED]).toDateString()}</p>
+                                            <p className="ml-1 flex-shrink-0 font-normal text-gray-500">Description: {job[JOB_DESCRIPTION]}</p>
+                                            <span className="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium bg-indigo-100 text-indigo-800">
+
+                                                {capitalize(tag)}
+
+                                            </span>
+                                        </div>
+                                        <div className="mt-2 flex">
+                                            <div className="flex items-center text-sm text-gray-500">
+                                                <CalendarIcon className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" aria-hidden="true" />
+
+
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <span className="inline-block relative">
+                                        <img
+                                            className="h-10 w-10 rounded-full"
+                                            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                                            alt=""
+                                        />
+                                        <span className="absolute top-0 right-0 block h-2.5 w-2.5 rounded-full ring-2 ring-white bg-green-400" />
+                                    </span>
+
+                                    <div className="ml-5 flex-shrink-0">
+                                        <ChevronRightIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+                                    </div>
+                                </div>
+                            </a></Link>
+                        </li>
                     ))
                 ) : (
                     <span className={"h-full flex justify-center items-center"}>
                         {isLoading ? 'Loading...' : `There are no ${capitalize(tag)} jobs yet!`}
                     </span>
                 )}
-            </div>
-        </div>
+
+            </ul>
+        </div >
+
     );
 }
 
