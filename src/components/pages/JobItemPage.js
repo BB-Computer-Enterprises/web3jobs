ef3a74b145d93e5c680b0204c46deb246ddc588e
Big B <8c57e820-f3a7-4561-a664-fed7d85bdedc@4wrd.cc>
2021-10-28 13:46:08 -0400

    individual job page is better now

--- src/components/pages/JobItemPage.js
+++ src/components/pages/JobItemPage.js
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import PageContainer from "../PageContainer";
 import {
     COMPANY_APPLICATION_URL,
     COMPANY_NAME,
@@ -24,14 +25,9 @@ const JobItemPage = job => {
         }
     } = job;
 
-    return (
-        <div>
-            <div className={"w-screen fixed flex flex-col min-h-screen bg-gray-50"}>
-                <header className={"flex justify-between items-center px-4 h-16 bg-gray-900"}>
-                    <span className={"text-2xl sm:text-4xl text-white border-b font-sans"}>
-                        Job Title:{`${jTitle}`}
-                    </span>
-                </header>
+    const getContent = () => {
+        return (
+            <div>
                 <p>
                     Description: {`${jDesc}`}
                 </p>
@@ -40,11 +36,17 @@ const JobItemPage = job => {
                 </p>
 
                 <Link to={JOBS_URL}>Back to All Jobs</Link>
-                {/*UPDATE THIS URL PATH TO POINT TO COMPANY PAGE*/}
-                <Link to={COMPANY_URL}>More jobs with {`${cName}`}</Link>
+                <p>
+                    {/*UPDATE THIS URL PATH TO POINT TO COMPANY PAGE*/}
+                    <Link to={COMPANY_URL}>More jobs with {`${cName}`}</Link>
+                </p>
             </div>
-        </div>
-    );
+        )
+    }
+
+    return (
+        PageContainer(getContent(), jTitle, jDesc)
+    )
 };
 
 export default JobItemPage;
