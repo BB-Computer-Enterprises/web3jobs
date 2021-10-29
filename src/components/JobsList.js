f61117f00cc60c1b663275a669cde5fca9c93ab8
Big B <8c57e820-f3a7-4561-a664-fed7d85bdedc@4wrd.cc>
2021-10-29 02:38:29 -0400

    slight style updates

--- src/components/JobsList.js
+++ src/components/JobsList.js
@@ -1,12 +1,10 @@
 
 import { Link } from "react-router-dom";
 import { capitalize, makeFriendlyUrl } from "@util/sanitize";
-import { CalendarIcon, ChevronRightIcon } from '@heroicons/react/solid'
 import {
     COMPANY_NAME,
     JOBS_URL,
     JOB_DATE_POSTED,
-    JOB_DESCRIPTION,
     JOB_ID,
     JOB_TITLE
 } from "@constants/";
@@ -18,28 +16,28 @@ const generateLinkURL = job => {
     const { [JOB_TITLE]: title, [JOB_ID]: id, companies: { [COMPANY_NAME]: cName } } = job;
     return `${JOBS_URL}/${makeFriendlyUrl(title)}-${makeFriendlyUrl(cName)}/${id}`
 }
-const sepratetags = job => {
-    return job.map(jobs => (
-        <span className="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium bg-indigo-100 text-indigo-800">
-            {jobs}
+
+const sepratetags = tags => {
+    return tags.map(tag => (
+        <span className="flex-1 items-center px-3 py-0.5 rounded-full text-sm font-medium bg-indigo-100 text-indigo-800">
+            {tag}
         </span>
     ))
 }
+
 const companyName = job => {
     const { companies: { [COMPANY_NAME]: cName } } = job;
     return cName
 }
+
 const JobsList = ({ jobs, isLoading, tag }) => {
     return (
-        <div className="bg-white shadow overflow-hidden sm:rounded-md pr-10 pl-10">
-            <ul role="list" className="divide-y divide-gray-200">
-
+        <div className="bg-gray-dark shadow-2xl overflow-hidden sm:rounded-md">
+            <ul className="divide-y divide-gray-200">
                 {jobs.length ? (
                     jobs.map((job) => (
-
                         <li key={job[JOB_ID]}>
-                            {console.log(job)}
-                            <Link to={{ pathname: generateLinkURL(job), state: { job } }}><a className="block hover:bg-gray-50">
+                            <Link to={{ pathname: generateLinkURL(job), state: { job } }} className="text-white block hover:bg-gray-light px-8 py-2">
                                 <div className="px-4 py-4 flex items-center sm:px-6">
                                     <span className="inline-block relative">
                                         <img
@@ -49,21 +47,13 @@ const JobsList = ({ jobs, isLoading, tag }) => {
                                         />
                                         <span className="absolute top-0 right-0 block h-2.5 w-2.5 rounded-full ring-2 ring-white bg-green-400" />
                                     </span>
-                                    <div className="min-w-0 flex-1 sm:flex sm:items-center sm:justify-between">
+                                    <div className="flex-1 pl-8 flex items-center justify-between">
                                         <div className="truncate">
-
-                                            <div className="flex text-sm"></div>
-                                            <Link to={{ pathname: generateLinkURL(job), state: { job } }}><h1 className="font-medium text-indigo-600 truncate">{job[JOB_TITLE]}</h1></Link>
+                                            <Link to={{ pathname: generateLinkURL(job), state: { job } }}><h1 className="font-medium text-xl text-indigo-600 truncate">{job[JOB_TITLE]}</h1></Link>
                                             <p className="ml-1 flex-shrink-0 font-normal text-gray-500 	font-medium"> {companyName(job)}</p>
-                                            <p className="ml-1 flex-shrink-0 font-normal text-gray-500 ">📅{new Date(job[JOB_DATE_POSTED]).toDateString()}</p>
-
+                                            <p className="ml-1 flex-shrink-0 font-normal text-gray-500 ">📅 {new Date(job[JOB_DATE_POSTED]).toDateString()}</p>
                                         </div>
-                                        <div className="mt-2 flex hidden lg:block">
-                                            {/* <div className="flex items-center text-sm text-gray-500">
-                                              <CalendarIcon className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" aria-hidden="true" />
-
-
-                                            </div> */}
+                                        <div className="inline-flex space-x-2 hidden lg:block">
                                             {sepratetags(job[JOB_TAGS])}
                                         </div>
                                         <button
@@ -80,7 +70,7 @@ const JobsList = ({ jobs, isLoading, tag }) => {
                                     </div> */}
 
                                 </div>
-                            </a></Link>
+                            </Link>
                         </li>
                     ))
                 ) : (
