b1dafc6b67b1eec4ad5584454b341cbf452f5aff
Brian Recuero <brianrecuero@gmail.com>
2021-10-29 02:38:58 -0400

    Updated All Jobs UI - Still Needs Work (#26)
    * Updated All Jobs UI - Still Needs Work
    
    * Updated Tags
    
    * Updated Jobs list UI
    
    * slight style updates
    
    Co-authored-by: B <uibmjh8e@anonaddy.me>
    Co-authored-by: Big B <8c57e820-f3a7-4561-a664-fed7d85bdedc@4wrd.cc>

--- src/components/JobsList.js
+++ src/components/JobsList.js
@@ -5,10 +5,10 @@ import {
     COMPANY_NAME,
     JOBS_URL,
     JOB_DATE_POSTED,
-    JOB_DESCRIPTION,
     JOB_ID,
     JOB_TITLE
 } from "@constants/";
+import { JOB_TAGS } from "@config/constants";
 
 // function that will destructure the job object
 // it pulls out the title, id and company name to be used in the URL
@@ -17,28 +17,71 @@ const generateLinkURL = job => {
     return `${JOBS_URL}/${makeFriendlyUrl(title)}-${makeFriendlyUrl(cName)}/${id}`
 }
 
+const sepratetags = tags => {
+    return tags.map(tag => (
+        <span className="flex-1 items-center px-3 py-0.5 rounded-full text-sm font-medium bg-indigo-100 text-indigo-800">
+            {tag}
+        </span>
+    ))
+}
+
+const companyName = job => {
+    const { companies: { [COMPANY_NAME]: cName } } = job;
+    return cName
+}
+
 const JobsList = ({ jobs, isLoading, tag }) => {
     return (
-        <div>
-            <div
-                className={`p-2 flex-grow grid gap-2 ${jobs.length ? "auto-rows-min" : ""
-                    } grid-cols-1 h-2/3 first:mt-8`}
-            >
+        <div className="bg-gray-dark shadow-2xl overflow-hidden sm:rounded-md">
+            <ul className="divide-y divide-gray-200">
                 {jobs.length ? (
                     jobs.map((job) => (
-                        <div className="bg-white px-4 py-5 border-b border-gray-200 sm:px-6" key={job[JOB_ID]} >
-                            <Link to={{ pathname: generateLinkURL(job), state: { job } }}><h1 className="text-lg leading-6 font-medium text-gray-900">Job Title: {job[JOB_TITLE]}</h1></Link>
-                            <p className="mt-1 text-sm text-gray-500">Created: {new Date(job[JOB_DATE_POSTED]).toDateString()}</p>
-                            <p className="mt-1 text-sm text-gray-500">Description: {job[JOB_DESCRIPTION]}</p>
-                        </div>
+                        <li key={job[JOB_ID]}>
+                            <Link to={{ pathname: generateLinkURL(job), state: { job } }} className="text-white block hover:bg-gray-light px-8 py-2">
+                                <div className="px-4 py-4 flex items-center sm:px-6">
+                                    <span className="inline-block relative">
+                                        <img
+                                            className="inline-block h-12 w-12 rounded-md"
+                                            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                                            alt=""
+                                        />
+                                        <span className="absolute top-0 right-0 block h-2.5 w-2.5 rounded-full ring-2 ring-white bg-green-400" />
+                                    </span>
+                                    <div className="flex-1 pl-8 flex items-center justify-between">
+                                        <div className="truncate">
+                                            <Link to={{ pathname: generateLinkURL(job), state: { job } }}><h1 className="font-medium text-xl text-indigo-600 truncate">{job[JOB_TITLE]}</h1></Link>
+                                            <p className="ml-1 flex-shrink-0 font-normal text-gray-500 	font-medium"> {companyName(job)}</p>
+                                            <p className="ml-1 flex-shrink-0 font-normal text-gray-500 ">📅 {new Date(job[JOB_DATE_POSTED]).toDateString()}</p>
+                                        </div>
+                                        <div className="inline-flex space-x-2 hidden lg:block">
+                                            {sepratetags(job[JOB_TAGS])}
+                                        </div>
+                                        <button
+                                            type="button"
+                                            className="inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 hidden lg:block"
+                                        >
+                                            Apply
+                                        </button>
+                                    </div>
+
+
+                                    {/* <div className="ml-5 flex-shrink-0">
+                                        <ChevronRightIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+                                    </div> */}
+
+                                </div>
+                            </Link>
+                        </li>
                     ))
                 ) : (
                     <span className={"h-full flex justify-center items-center"}>
                         {isLoading ? 'Loading...' : `There are no ${capitalize(tag)} jobs yet!`}
                     </span>
                 )}
-            </div>
-        </div>
+
+            </ul>
+        </div >
+
     );
 }
 
