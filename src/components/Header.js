03b2f35e9315098f9bfff9f08af4db9abd385bb4
B Nobel <8c57e820-f3a7-4561-a664-fed7d85bdedc@4wrd.cc>
2021-10-29 18:28:44 -0400

    Sticky header (#31)

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,7 +20,7 @@ const linkStyle = "text-base text-2xl font-medium text-white hover:text-white";
 
 export default function Header() {
     return (
-        <header className="bg-gray-dark">
+        <header className="bg-gray-dark sticky top-0 z-50">
             <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8" aria-label="Top">
                 <div className="w-full py-6 flex items-center justify-between border-b border-indigo-500 lg:border-none">
                     <div className="flex items-center">
