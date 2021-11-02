a4ac7e48b23b888b7d78d415da5bdd315a1e3f11
Big B <8c57e820-f3a7-4561-a664-fed7d85bdedc@4wrd.cc>
2021-11-01 23:49:26 -0400

    icon brings you home

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,3 @@
-import { Popover } from '@headlessui/react'
 import {
     HOME_LINK,
     COMPANIES_LINK,
@@ -24,7 +23,7 @@ export default function Header() {
             <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8" aria-label="Top">
                 <div className="w-full py-6 flex items-center justify-between border-b border-indigo-500 lg:border-none">
                     <div className="flex items-center">
-                        <a href="#">
+                        <a href="/">
                             <span className="sr-only">Workflow</span>
                             <img
                                 className="h-10 w-auto"
